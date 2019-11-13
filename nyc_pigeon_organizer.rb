62b6cae8a3bfea3586db0a309dfc98a07913fab2
Andrew Richards <arichards4814@gmail.com>
2019-11-13 13:11:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+newHash = {}
+
+pigeon_data.each do |key, value|
+    value.each do |key2, value2|
+      value2.map do |ele|
+        
+        if newHash[ele] == nil
+          #create empty k v pair for name
+          newHash[ele] = {}
+        end
+
+        if newHash[ele][key] == nil
+          #create empty k v pair for attribute
+          newHash[ele][key] = []
+        end
+          
+          #push the news values into our new array
+        newHash[ele][key].push(key2.to_s)
+
+      end
+      
+    end
+    
+end
+
+
+newHash
 end
